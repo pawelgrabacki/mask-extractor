6788396a8f3368b8fe4a230d3e03544c4ea15f2c
Paweł Grabacki <paw.grabacki@gmail.com>
2025-05-25 23:21:53 +0200

    update

--- src/mask_extractor.py
+++ src/mask_extractor.py
@@ -1,22 +1,28 @@
-from PIL import Image
+"""Script"""
+
 import os
 import time
+from PIL import Image
+
 
 # Extracts the alpha channel from PNG files and saves it as a separate mask image.
 
+
 def create_mask():
+    """Creates mask"""
     found = False
-    for f in os.listdir('.'):
-        if f.lower().endswith('.png'):
+    for f in os.listdir("."):
+        if f.lower().endswith(".png"):
             found = True
-            img = Image.open(f).convert('RGBA')
+            img = Image.open(f).convert("RGBA")
             alpha = img.getchannel("A")
             fn, _ = os.path.splitext(f)
-            alpha.save(f'masks/{fn}_mask.png')
+            alpha.save(f"masks/{fn}_mask.png")
             print(f"{f} is done")
-    
+
     if not found:
         print("No PNG files found.")
+
 
 # Create 'masks' directory if it doesn't exist
 os.makedirs("masks", exist_ok=True)
